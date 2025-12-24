2cab69635634f5ee8e079c006ed09af6b371f954
Chris DeFrancisci <chrisdefrancisci@gmail.com>
2025-12-24 18:42:42 -0500

    first commit

---
+++ quantized_looper/Debug/Core/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../Core/main.cpp 
+
+OBJS += \
+./Core/main.o 
+
+CPP_DEPS += \
+./Core/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/%.o Core/%.su Core/%.cyclo: ../Core/%.cpp Core/subdir.mk
+	arm-none-eabi-g++ "$<" -mcpu=cortex-m7 -std=gnu++14 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F767xx -c -I../Core/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F7xx/Include -I../Drivers/CMSIS/Include -I"/home/chris/Documents/Projects/Synth/software/quantized_looper/quantized_looper" -O0 -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti -fno-use-cxa-atexit -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core
+
+clean-Core:
+	-$(RM) ./Core/main.cyclo ./Core/main.d ./Core/main.o ./Core/main.su
+
+.PHONY: clean-Core
+
