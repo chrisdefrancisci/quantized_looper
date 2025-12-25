b00e017530eef8bd985392b98c939bb1f82b8f89
Chris DeFrancisci <chrisdefrancisci@gmail.com>
2025-12-24 20:09:32 -0500

    Back to where we started... but at least now we have a working initial blinking project

--- quantized_looper/main.cpp
+++ quantized_looper/main.cpp
@@ -1,46 +1,28 @@
-/*
- * main.cpp
- *
- *  Created on: Dec 21, 2025
- *      Author: chris
- */
-
-// #include <quantized_looper/Core/Inc/main.h>
-// #include <quantized_looper/Core/Inc/gpio.h>
 #include <main.h>
 #include <gpio.h>
 
-#include <quantized_looper/Hardware/led.hpp>
 
 extern "C"
 {
-	extern void SystemClock_Config();
+   extern void SystemClock_Config();
 }
-
-template <uint32_t port, uint16_t pin>
-class port_pin
-{
-public:
-	static void set_pin_high() { HAL_GPIO_WritePin(reinterpret_cast<GPIO_TypeDef*>(port), pin, GPIO_PIN_SET); }
-	static void set_pin_low() { HAL_GPIO_WritePin(reinterpret_cast<GPIO_TypeDef*>(port), pin, GPIO_PIN_RESET); }
-};
 
 int main()
 {
-	HAL_Init();
+   HAL_Init();
 
-	SystemClock_Config();
 
-	MX_GPIO_Init();
+   SystemClock_Config();
 
-	while(true)
-	{
-		port_pin<GPIOB_BASE, LD2_Pin>::set_pin_high();
-		HAL_Delay(2000);
-		port_pin<GPIOB_BASE, LD2_Pin>::set_pin_low();
-		HAL_Delay(1000);
-	}
+
+   MX_GPIO_Init();
+
+
+   while(true)
+   {
+       HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+       HAL_Delay(200);
+       HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+       HAL_Delay(100);
+   }
 }
-
-
-
