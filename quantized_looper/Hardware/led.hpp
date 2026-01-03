afa4f059f1644fae9e9a4ac6f054ffb7c2d662a4
Chris DeFrancisci <chrisdefrancisci@gmail.com>
2026-01-03 13:57:55 -0500

    Added .clang-format

--- quantized_looper/Hardware/led.hpp
+++ quantized_looper/Hardware/led.hpp
@@ -1,7 +1,7 @@
 /**
  * @file led.hpp
  * @author Chris DeFrancisci (chrisdefrancisci@gmail.com)
- * @brief 
+ * @brief
  * @date 2025-12-27
  */
 
@@ -19,52 +19,42 @@
 
 /**
  * @brief Template specialization for LED controlled by GPIO.
- * 
- * @tparam GPIO_TypeDef  
+ *
+ * @tparam GPIO_TypeDef
  */
 template<>
 class led<GPIO_TypeDef> : public ledBase
 {
 public:
-    
     /**
-     * @brief Construct a new led object 
-     * 
+     * @brief Construct a new led object
+     *
      * @param handle GPIO handle
      * @param pin Pin number
      */
-    led(GPIO_TypeDef* handle, unsigned int pin) :
-        handle(handle),
-        pin(pin)
-    {
-    };
+    led(GPIO_TypeDef* handle, unsigned int pin)
+      : handle(handle)
+      , pin(pin) {};
 
-    void on() override
-    {
-        HAL_GPIO_WritePin(handle, pin, GPIO_PIN_SET);
-    }
+    void on() override { HAL_GPIO_WritePin(handle, pin, GPIO_PIN_SET); }
 
-    void off() override
-    {
-        HAL_GPIO_WritePin(handle, pin, GPIO_PIN_RESET);
-    }
+    void off() override { HAL_GPIO_WritePin(handle, pin, GPIO_PIN_RESET); }
 
     void setIntensity(int value) override
-    { 
-        if (value != 0)
-        {
+    {
+        if (value != 0) {
             on();
-        }
-        else 
-        {
+        } else {
             off();
         }
     }
-    
-    void setIntensity(float value) override { setIntensity(value > 0); }
-    
-    std::pair<int, int> getRange() const override { return std::pair<int, int>(0, 1); }
 
+    void setIntensity(float value) override { setIntensity(value > 0); }
+
+    std::pair<int, int> getRange() const override
+    {
+        return std::pair<int, int>(0, 1);
+    }
 
 private:
     GPIO_TypeDef* handle;
@@ -74,62 +64,55 @@ private:
 
 /**
  * @brief Template specialization for LED controlled by PWM.
- * 
+ *
  * @tparam TIM_HandleTypeDef
  */
 template<>
 class led<TIM_HandleTypeDef> : public ledBase
 {
 public:
-    
     /**
      * @brief Construct a new led object
-     * 
+     *
      * @param handle PWM handle
      * @param channel PWM channel
      * @param constructor PWM init function
      * @param destructor PWM deinit function
      */
-    led(TIM_HandleTypeDef* handle, unsigned int channel, 
-        std::function<void()> constructor = [](){}, std::function<void()> destructor = [](){}) :
-        handle(handle),
-        channel(channel),
-        constructor(constructor),
-        destructor(destructor)
+    led(
+      TIM_HandleTypeDef* handle,
+      unsigned int channel,
+      std::function<void()> constructor = []() {},
+      std::function<void()> destructor = []() {})
+      : handle(handle)
+      , channel(channel)
+      , constructor(constructor)
+      , destructor(destructor)
     {
         constructor();
         range.first = 0;
         range.second = handle->Init.Period;
     };
 
-    virtual ~led() 
-    {
-        destructor();
-    };
+    virtual ~led() { destructor(); };
 
-    void on() override
-    {
-        HAL_TIM_PWM_Start(handle, channel);
-    }
+    void on() override { HAL_TIM_PWM_Start(handle, channel); }
 
-    void off() override
-    {
-        HAL_TIM_PWM_Stop(handle, channel);
-    }
+    void off() override { HAL_TIM_PWM_Stop(handle, channel); }
 
     void setIntensity(int value) override
-    { 
+    {
         off();
         // Ensure value is within range
         value = value > range.second ? range.second : value;
         value = value < range.first ? range.first : value;
-   
+
         handle->Instance->CCR3 = value;
         on();
     }
 
     void setIntensity(float value) override
-    { 
+    {
         int valueInt = (range.second - range.first) * value - range.first;
         setIntensity(valueInt);
     }
@@ -143,4 +126,3 @@ private:
     std::function<void()> destructor;
     std::pair<int, int> range;
 };
-
--- quantized_looper/main.cpp
+++ quantized_looper/main.cpp
@@ -1,19 +1,26 @@
 #include <memory>
 #include <vector>
 
-#include <main.h>
 #include <gpio.h>
+#include <main.h>
 #include <tim.h>
 
-#include <quantized_looper/Hardware/led.hpp>
 #include "stm32f767xx.h"
+#include <quantized_looper/Hardware/led.hpp>
 
 extern "C"
 {
-   extern void SystemClock_Config();
+    extern void SystemClock_Config();
 }
 
-int main()
+void
+test()
+{
+    int* a;
+};
+
+int
+main()
 {
     HAL_Init();
 
@@ -24,27 +31,20 @@ int main()
 
     std::vector<std::unique_ptr<ledBase>> leds;
     leds.push_back(std::make_unique<led<TIM_HandleTypeDef>>(
-        &htim3, TIM_CHANNEL_3, MX_TIM3_Init, MX_TIM3_DeInit)
-    );
+      &htim3, TIM_CHANNEL_3, MX_TIM3_Init, MX_TIM3_DeInit));
 
     leds.push_back(std::make_unique<led<GPIO_TypeDef>>(LD2_GPIO_Port, LD2_Pin));
     leds.push_back(std::make_unique<led<GPIO_TypeDef>>(LD3_GPIO_Port, LD3_Pin));
 
-        
     int analogValue = 0;
     int increment = 6000;
     int onIdx = 0;
 
-    while(true)
-    {
-        for (int i = 0; i < leds.size(); i++)
-        {
-            if (i == onIdx)
-            {
+    while (true) {
+        for (int i = 0; i < leds.size(); i++) {
+            if (i == onIdx) {
                 leds[i]->setIntensity(analogValue);
-            }
-            else
-            {
+            } else {
                 leds[i]->off();
             }
         }
@@ -54,17 +54,14 @@ int main()
         onIdx = onIdx >= leds.size() ? 0 : onIdx;
 
         analogValue += increment;
-        if (analogValue > leds[0]->getRange().second || analogValue < leds[0]->getRange().first)
-        {
-            for(int i = 0; i < 4; i++)
-            {
-                for (auto&& led : leds)
-                {
+        if (analogValue > leds[0]->getRange().second ||
+            analogValue < leds[0]->getRange().first) {
+            for (int i = 0; i < 4; i++) {
+                for (auto&& led : leds) {
                     led->on();
                 }
                 HAL_Delay(300);
-                for (auto&& led : leds)
-                {
+                for (auto&& led : leds) {
                     led->off();
                 }
                 HAL_Delay(100);
